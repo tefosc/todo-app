b0846cc63e6d6bd872b0bca89c5311555d87bd77
Alejandro Stefano Solis Martos <asolism17_1@unc.edu.pe>
2023-11-08 19:15:03 -0500

    ust delete signs that were not used

--- src/CompletTodo.jsx
+++ src/CompletTodo.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
+
 function CompletTodo({ item }) {
   const [status, setStatus] = useState(false);
+
   return (
-    <>
-      <a
-        href="#"
-        onClick={() => {
-          setStatus(!status);
-        }}
-        className={status ? "line-through" : ""}
-      >
-        {item.text}
-      </a>
-    </>
+    <a
+      href="#"
+      onClick={() => {
+        setStatus(!status);
+      }}
+      className={status ? "line-through" : ""}
+    >
+      {item.text}
+    </a>
   );
 }
 
